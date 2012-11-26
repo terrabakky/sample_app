6b4d9f081040dce0013831e211fdb0ea6a6d33c2
Ekko <tyler.kieran.johnson@gmail.com>
2012-11-27 00:40:36 +1100

    fixed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ SampleApp::Application.routes.draw do
   match '/help',	:to => 'static_pages#help'
   match '/about',	:to => 'static_pages#about'
   match '/contact',	:to => 'static_pages#contact'
-  
+  match '/signup', 	:to => 'users#new'
+
   get "static_pages/home"
 
   get "static_pages/help"
